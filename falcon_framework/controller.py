d8e5341836369da5415752e7adfbf021654bdfa2
Matthias Kiendl <matthias.kiendl@student.uni-augsburg.de>
2025-07-21 02:37:13 +0200

    find_timezone_city added, import Nominatim from geocoders

--- falcon_framework/controller.py
+++ falcon_framework/controller.py
@@ -1,7 +1,7 @@
 import falcon
 import requests
-from geopy.geocoders import Nominatim
-from timezonefinder import TimezoneFinder
+from geopy.geocoders import Nominatim                               #city -> coordinates
+from timezonefinder import TimezoneFinder                           #coordinates -> timezone
 
 # TODO: Logik Zeitzonen herausfinden
 def find_timezone_ip() -> str | None:
@@ -10,10 +10,19 @@ def find_timezone_ip() -> str | None:
         b_grad = info.get("latitude")
         l_grad = info.get("longitude")
         if b_grad and l_grad:
-            finder = TimezoneFinder()
-            return finder.timezone_at(lat=b_grad, lng=l_grad)
+            return TimezoneFinder().timezone_at(lng=l_grad, lat=b_grad)
     except Exception:
         return None
+
+def find_timezone_city(cityname: str) -> str | None:
+        geolocator = Nominatim(user_agent="timezone_city")
+        location = geolocator.geocode(cityname)
+        if not location:
+            return None
+        b_grad = location.latitude
+        l_grad = location.langitude
+        return TimezoneFinder().timezone_at(lng=l_grad, lat=b_grad)
+
 
 standard_url = "http://php/index.php?action="
 
@@ -34,7 +43,7 @@ class WhatTimeIsIt:
         php_response = requests.get(standard_url + "whattimeisit", params={"timezone": time_zone})
         resp.media = php_response.json()
 
-# Get und Post mit /in
+# Get und Post mit /in --- Eingabe möglich 
 class InByCityGet:
     def on_get(self, req, resp, city):
         resp.media = {"n/a": "TODO"}
